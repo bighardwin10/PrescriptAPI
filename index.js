e07167818555b59cf54f4423787142fa1a1ba28f
BigHardWin10 <win_tree@163.com>
2026-03-22 14:20:02 +0800

    新增访问/prescripts一次获取所有指令

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const router = AutoRouter()
 //     event.respondWith(router.ha)
 // })
 
-router.get("/",checkService).get("/prescript",getPrescript)
+router.get("/",checkService).get("/prescript",getPrescript).get("/prescripts",getPrescripts)
 
 async function checkService() {
     return new Response(JSON.stringify({"status": "success","data": "","message": "服务正常运行"}),{headers: { 'Content-Type': 'application/json','Access-Control-Allow-Origin': '*' }})
 }
 
 async function getPrescript() {
-    const resp = await fetch("https://cdn.jsdelivr.net/gh/WIN-TREE/PrescriptAPI@latest/prescripts.json")
+    const resp = await fetch("https://cdn.jsdelivr.net/gh/bighardwin10/PrescriptAPI@latest/prescripts.json")
     const prescripts = await resp.json()
     return new Response(JSON.stringify({"status": "success","data": prescripts[Math.floor(Math.random() * prescripts.length)],"message": "部分指令由Deepseek编写，剩余来自xiaomu1999f-cpu.github.io与nyos.dev"}),{headers: { 'Content-Type': 'application/json','Access-Control-Allow-Origin': '*' }})
+}
+
+async function getPrescripts() {
+    const resp = await fetch("https://cdn.jsdelivr.net/gh/bighardwin10/PrescriptAPI@latest/prescripts.json")
+    return new Response(JSON.stringify({"status": "success","data": await resp.json}))
 }
 
 export default router
